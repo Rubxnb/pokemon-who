93b6c9f9ed8fbe33195ee65edf6b4e29aa8868a5
Rubxn <rubenbm198@gmail.com>
2023-07-09 19:33:49 +0200

    capitalize names

--- src/helpers/getPokemonOptions.js
+++ src/helpers/getPokemonOptions.js
@@ -32,11 +32,15 @@ const getPokemonNames = async ([a, b, c, d] = []) => {
     const [p1, p2, p3, p4] =  await Promise.all( promiseArr )
 
     return [
-        { id: a, name: p1.data.name},
-        { id: b, name: p2.data.name},
-        { id: c, name: p3.data.name},
-        { id: d, name: p4.data.name}
+        { id: a, name: capitalizeFirstLetter(p1.data.name)},
+        { id: b, name: capitalizeFirstLetter(p2.data.name)},
+        { id: c, name: capitalizeFirstLetter(p3.data.name)},
+        { id: d, name: capitalizeFirstLetter(p4.data.name)}
     ]
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 export default getPokemonOptions
